0f4e38e4f77d824f0b9070f6cf1b510aa73981d1
eldaniii <dvilmar309@g.educaand.es>
2024-12-17 10:06:55 +0100

    Subida archivos

--- U4/U4T1/U4T1_dvm.js
+++ U4/U4T1/U4T1_dvm.js
@@ -1,7 +1,16 @@
 // U4T1 - ENTREGABLE 1
 
 function validar(e) {
-    e.preventDefault()  
+    let inputNombre=document.getElementById("nombre");
+    let inputDni=document.getElementById("dni");
+    let inputFabricante=document.getElementById("fabricante");
+    let inputPassword=document.getElementById("password");
+    let valido=false;
+
+    e.preventDefault();
+
+    if (inputNombre!=null || inputDni==null || inputFabricante==null || inputPassword==null) valido=false;
+    else (nombre.type!=text);
 };
 
 // Daniel Vilar Martínez
